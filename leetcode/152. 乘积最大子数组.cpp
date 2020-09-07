c3b74bccfa18c1a75936a8c12727fc16d5014bd4
xuhuasheng <xu_watson@163.com>
2020-09-07 15:46:32 +0800

    # 20200907

---
+++ leetcode/152. 乘积最大子数组.cpp
@@ -0,0 +1,30 @@
+// # 给你一个整数数组 nums ，请你找出数组中乘积最大的连续子数组（该子数组中至少包含一个数字），并返回该子数组所对应的乘积。
+
+// # 示例 1:
+
+// # 输入: [2,3,-2,4]
+// # 输出: 6
+// # 解释: 子数组 [2,3] 有最大乘积 6。
+// # 示例 2:
+
+// # 输入: [-2,0,-1]
+// # 输出: 0
+// # 解释: 结果不能为 2, 因为 [-2,-1] 不是子数组。
+
+// # 来源：力扣（LeetCode）
+// # 链接：https://leetcode-cn.com/problems/maximum-product-subarray
+// # 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> dp_max(nums);
+        vector<int> dp_min(nums);
+        for (int i=1; i<n; i++) {
+            dp_max[i] = max(dp_max[i-1] * nums[i], max(dp_min[i-1] * nums[i], nums[i]));
+            dp_min[i] = min(dp_max[i-1] * nums[i], min(dp_min[i-1] * nums[i], nums[i]));
+        }
+        return *max_element(dp_max.begin(), dp_max.end());
+    }
+};
