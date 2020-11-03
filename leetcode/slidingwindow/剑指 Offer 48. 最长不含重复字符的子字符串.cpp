1a17730ba68ac77af2a7b67314ff3638a90425d9
xuhuasheng <xu_watson@163.com>
2020-11-03 18:42:23 +0800

    # 1103

--- leetcode/slidingwindow/剑指 Offer 48. 最长不含重复字符的子字符串.cpp
+++ leetcode/slidingwindow/剑指 Offer 48. 最长不含重复字符的子字符串.cpp
@@ -1,4 +1,6 @@
-// 给定一个字符串，请你找出其中不含有重复字符的 最长子串 的长度。
+// 请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+
+//  
 
 // 示例 1:
 
@@ -18,25 +20,32 @@
 //      请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
 
 // 来源：力扣（LeetCode）
-// 链接：https://leetcode-cn.com/problems/longest-substring-without-repeating-characters
+// 链接：https://leetcode-cn.com/problems/zui-chang-bu-han-zhong-fu-zi-fu-de-zi-zi-fu-chuan-lcof
 // 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        unordered_set<char> check;
         int n = s.size();
-        int right = 0, ans = 0;
-        for (int left=0; left<n; left++) {
-            if (left != 0) {
-                check.erase(s[left-1]);
+        if (n==0) return 0;
+        int cnt = 0;
+        int ans = 0;
+        unordered_map<char, int> log;
+        for (int i=0; i<s.size(); i++) {
+            if (log.find(s[i]) == log.end()) {
+                cnt += 1;
+                log.insert(pair<char, int> {s[i], i});
+            } else {
+                int d = i - log[s[i]];
+                if (d > cnt)
+                    cnt += 1;
+                else
+                    cnt = d;
             }
-            while (right < n && !check.count(s[right])) {
-                check.insert(s[right]);
-                right ++;
-            }
-            ans = max(ans, right - left);
+            log[s[i]] = i;
+            ans = max(ans, cnt);
         }
-        return ans
+        return ans;
+
     }
-}
+};
